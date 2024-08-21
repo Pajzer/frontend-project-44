fbd1c84bfc2bf96b8b873b64acf6e17a4a02d950
Максим Лысов <pajzer@gmail.com>
2024-08-21 19:43:08 +0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ const runGame = (description, gameRound) => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${username}!`);
-  
   // Описание правил игры
   console.log(description);
   for (let i = 0; i < stepsCount; i += 1) {
     const { correctAnswer, question } = gameRound();
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
-
     // Сверяет ответ пользователя с правильным ответом
     if (answer.toLowerCase() === correctAnswer) {
       console.log('Correct!');
@@ -23,7 +21,6 @@ const runGame = (description, gameRound) => {
       return;
     }
   }
-  
   console.log(`Congratulations, ${username}!`);
 };
 
