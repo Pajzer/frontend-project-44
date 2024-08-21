37ddcf5548a26bf344ca47b6dd4ff5bd028dfecf
Максим Лысов <pajzer@gmail.com>
2024-08-21 17:42:35 +0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@ import readlineSync from 'readline-sync';
 const stepsCount = 3;
 
 const runGame = (description, gameRound) => {
-	console.log('Welcome to the Brain Games!');
-	const username = readlineSync.question('May I have your name?: ');
-	console.log(`Hello, ${username}!`);
-	// Описание правил игры
-    console.log(description);
+  console.log('Welcome to the Brain Games!');
+  const username = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${username}!`);
+  // Описание правил игры
+  console.log(description);
 
-	for (let i = 0; i < stepsCount; i ++) {
-        const {correctAnswer, question } = gameRound();
+  for (let i = 0; i < stepsCount; i += 1) {
+    const { correctAnswer, question } = gameRound();
 
-		console.log(`Question: ${question}`);
-		const answer = readlineSync.question('Your answer: ');
-		
-		// Сверяет ответ пользователя с правильным ответом
-		if (answer.toLowerCase() === correctAnswer) {
-			console.log('Correct!');
-		} else {
-			console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-			console.log(`Let's try again, ${username}!`);
-			return
-		}
-	}	
-	console.log(`Congratulations, ${username}!`);
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    // Сверяет ответ пользователя с правильным ответом
+    if (answer.toLowerCase() === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${username}!`);
+      return;
+    }
+  }	
+  console.log(`Congratulations, ${username}!`);
 };
 
 export default runGame;
